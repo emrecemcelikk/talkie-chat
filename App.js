8232aafac4977567935fb7c956f1f276c4a15291
= <=>
2021-11-01 11:47:56 +0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {useFonts} from 'expo-font';
 
 export default function App() {
+  const [loaded] = useFonts({
+    MontserratBold: require('./assets/fonts/Montserrat-Bold.ttf'),
+    Pacifico: require('./assets/fonts/Pacifico-Regular.ttf'),
+  });
+  
+  if (!loaded) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>talkie</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +23,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffe194',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontFamily:'Pacifico',
+    fontSize:50,
+  }
 });
